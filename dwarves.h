930bb045b98a5b0ddbc2db2aebe2ff7e7c77bca0
Alan Maguire <alan.maguire@oracle.com>
2022-09-23 11:29:39 +0100

    dwarves: help dwarf loader recognize optimized-out parameters
    Compilation generates DWARF at several stages, and often the
    later DWARF representations more accurately represent optimizations
    that have occurred during compilation.
    
    In particular, parameter representations can be spotted by their
    abstract origin references to the original parameter, but they
    often have more accurate location information.  In most cases,
    the parameter locations will match calling conventions, and be
    registers for the first 6 parameters on x86_64, first 8 on ARM64
    etc.  If the parameter is not a register when it should be however,
    it is likely passed via the stack or the compiler has used a
    constant representation instead.
    
    This change adds a field to parameters and their associated
    ftype to note if a parameter has been optimized out.  Having
    this information allows type encoding to make a decision on
    whether such parameters should be encoded.
    
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>

--- dwarves.h
+++ dwarves.h
@@ -789,6 +789,7 @@ size_t lexblock__fprintf(const struct lexblock *lexblock, const struct cu *cu,
 struct parameter {
 	struct tag tag;
 	const char *name;
+	bool optimized;
 };
 
 static inline struct parameter *tag__parameter(const struct tag *tag)
@@ -808,7 +809,8 @@ struct ftype {
 	struct tag	 tag;
 	struct list_head parms;
 	uint16_t	 nr_parms;
-	uint8_t		 unspec_parms; /* just one bit is needed */
+	uint8_t		 unspec_parms:1; /* just one bit is needed */
+	uint8_t		 optimized_parms:1;
 };
 
 static inline struct ftype *tag__ftype(const struct tag *tag)
