d365b823f32a4f2085eda86f2448d40faea4f161
Yonghong Song <yonghong.song@linux.dev>
2026-05-23 09:57:33 -0700

    dwarf_loader: Refactor initial ret -1 to be macro PARM_DEFAULT_FAIL
    Later on, More macro return values will be implemented to make
    code easier to understand.
    
    Signed-off-by: Yonghong Song <yonghong.song@linux.dev>

--- dwarf_loader.c
+++ dwarf_loader.c
@@ -1190,6 +1190,8 @@ static ptrdiff_t __dwarf_getlocations(Dwarf_Attribute *attr,
 	return ret;
 }
 
+#define	PARM_DEFAULT_FAIL	-1
+
 /* Max 20 register parameters, considering some parameters may be optimized out.  */
 #define	MAX_PRESCAN_PARAMS	20
 
@@ -1281,7 +1283,7 @@ static int parameter__peek_first_reg(Dwarf_Die *die)
  *   otherwise save the register for later return
  * - if location DW_OP_entry_value(DW_OP_regXX) with expected number is in the
  *   list, return the register; otherwise save register for later return
- * - otherwise if no register was found for locations, return -1.
+ * - otherwise if no register was found for locations, return PARM_DEFAULT_FAIL.
  */
 static int parameter__reg(Dwarf_Attribute *attr, int expected_reg)
 {
@@ -1291,7 +1293,7 @@ static int parameter__reg(Dwarf_Attribute *attr, int expected_reg)
 	size_t exprlen, entry_len;
 	ptrdiff_t offset = 0;
 	int loc_num = -1;
-	int ret = -1;
+	int ret = PARM_DEFAULT_FAIL;
 
 	/* use libdw__lock as dwarf_getlocation(s) has concurrency issues
 	 * when libdw is not compiled with experimental --enable-thread-safety
@@ -1403,7 +1405,7 @@ static struct parameter *parameter__new(Dwarf_Die *die, struct cu *cu,
 			int expected_reg = cu->register_params[reg_idx];
 			int actual_reg = parameter__reg(&attr, expected_reg);
 
-			if (actual_reg < 0)
+			if (actual_reg == PARM_DEFAULT_FAIL)
 				parm->optimized = 1;
 			else if (expected_reg >= 0 && expected_reg != actual_reg)
 				/* mark parameters that use an unexpected
