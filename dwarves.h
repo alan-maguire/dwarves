d7f3fa584c2afae9a5ba76324ab746324e7e3f2c
Alan Maguire <alan.maguire@oracle.com>
2026-06-21 14:23:23 +0100

    dwarf_loader: Add parameter list to inlined expansions
    DW_TAG_formal_parameter children of DW_TAG_inlined_subroutine are
    currently skipped, so callers cannot iterate parameters that belong to a
    specific inline expansion. Store those parameters directly on struct
    inline_expansion instead of leaving them in the surrounding lexical block.
    
    Add an inline_expansion parameter list, populate it while processing
    inline-subroutine children, and include it in tag cleanup and
    cu__for_all_tags() traversal. Recode inline-expansion parameter types via
    the same parameter recoding path used for function prototypes.
    
    Signed-off-by: Thierry Treyer <ttreyer@meta.com>
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>

--- dwarves.h
+++ dwarves.h
@@ -830,6 +830,8 @@ struct inline_expansion {
 	struct ip_tag	 ip;
 	size_t		 size;
 	uint64_t	 high_pc;
+	struct list_head parms;
+	uint16_t	 nr_parms;
 };
 
 static inline struct inline_expansion *
@@ -837,6 +839,10 @@ static inline struct inline_expansion *
 {
 	return (struct inline_expansion *)tag;
 }
+
+struct parameter;
+void inline_expansion__add_parameter(struct inline_expansion *exp,
+				     struct parameter *parm);
 
 struct label {
 	struct ip_tag	 ip;
